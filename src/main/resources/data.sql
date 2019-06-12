a5c622f285aadfcfabf3bc867619ef920cb40568
amir-bunjo <amirbunjo@Amirs-MacBook-Pro.local>
2019-06-13 00:34:38 +0200

    updated restaurant controller and  added sql database files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,40 @@
+
+
+INSERT INTO country (name) VALUES ('India');
+INSERT INTO country (name) VALUES ('Brazil');
+INSERT INTO country (name) VALUES ('USA');
+INSERT INTO country (name) VALUES ('Italy');
+
+
+INSERT INTO users (first_name,last_name,email,country,city,account_role,password,phone)
+ VALUES ('Amir','Bunjo','bunjo16@gmail.com','BiH','Sarajevo','USER','YnVuam8xMjM=','38762360120');
+
+
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (1,'Vatra','Ferhadija','/assets/img/restaurant-promo/vatra.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (1,'Chipas','Strosmajerova','/assets/img/restaurant-promo/chipas.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (1,'Four Seasons','Marijin Dvor 1','/assets/img/restaurant-promo/four-seasons.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (1,'Vapiano','Marijin Dvor 2','/assets/img/restaurant-promo/vapiano.jpeg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (1,'Zmaj','Autobuska','/assets/img/restaurant-promo/zmaj.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (2,'Pacific','Josipa Bana Jelacica','/assets/img/restaurant-promo/pacific.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (1,'Radon Plaza','Stup 2','/assets/img/restaurant-promo/radon.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (1,'Brajlovic','Samira Ćatovića Kobre 6','/assets/img/restaurant-promo/brajlovic.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (3,'Nanina kuhinja','Kundurdžiluk 35','/assets/img/restaurant-promo/nanina.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (3,'Vrelo Bosne','Magistrala 66, Ilidza','/assets/img/restaurant-promo/vrelo-bosne.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (3,'Konyali Ahmet Usta','Kundurdžiluk','/assets/img/restaurant-promo/konyali.jpeg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (3,'Ćevabdžinica Mrkva','Bravadžiluk 13','/assets/img/restaurant-promo/mrkva.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (3,'Fabrika dobre hrane','Milana Preloga','/assets/img/restaurant-promo/fabrika.jpeg' );
+
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (1,'Vatra','Ferhadija','/assets/img/restaurant-promo/vatra.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (1,'Chipas','Strosmajerova','/assets/img/restaurant-promo/chipas.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (1,'Four Seasons','Marijin Dvor 1','/assets/img/restaurant-promo/four-seasons.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (1,'Vapiano','Marijin Dvor 2','/assets/img/restaurant-promo/vapiano.jpeg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (1,'Zmaj','Autobuska','/assets/img/restaurant-promo/zmaj.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (2,'Pacific','Josipa Bana Jelacica','/assets/img/restaurant-promo/pacific.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (1,'Radon Plaza','Stup 2','/assets/img/restaurant-promo/radon.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (1,'Brajlovic','Samira Ćatovića Kobre 6','/assets/img/restaurant-promo/brajlovic.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (3,'Nanina kuhinja','Kundurdžiluk 35','/assets/img/restaurant-promo/nanina.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (3,'Vrelo Bosne','Magistrala 66, Ilidza','/assets/img/restaurant-promo/vrelo-bosne.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (3,'Konyali Ahmet Usta','Kundurdžiluk','/assets/img/restaurant-promo/konyali.jpeg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (3,'Ćevabdžinica Mrkva','Bravadžiluk 13','/assets/img/restaurant-promo/mrkva.jpg' );
+INSERT INTO restaurants (city_id,name,street,promo_photo) VALUES (3,'Fabrika dobre hrane','Milana Preloga','/assets/img/restaurant-promo/fabrika.jpeg' );
+
